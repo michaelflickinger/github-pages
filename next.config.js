9cdcad720d7ce29764059538bf058e6384885b8e
Michael Flickinger <michael@madeby.holiday>
2024-07-17 20:22:04 -0700

    another config

--- next.config.js
+++ next.config.js
@@ -7,6 +7,10 @@
 const nextConfig = {
   // Use the CDN in production and localhost for development.
   // assetPrefix: isProd ? '/github-pages/' : undefined,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
   basePath: '/github-pages',
   assetPrefix: '/github-pages/',
 }
